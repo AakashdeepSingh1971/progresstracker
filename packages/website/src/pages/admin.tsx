fd779f0344b2373ab0e12aad269f18463832c134
aakashdeep singh <aakashdeep01971@gmail.com>
2021-08-28 14:36:33 +0530

    done some progress

--- packages/website/src/pages/admin.tsx
+++ packages/website/src/pages/admin.tsx
@@ -42,13 +42,13 @@ export default function Home() {
         <div className="w-1/4 sticky-right-0 shadow-lg" >
           <div className="m-4">
             <div className=" "></div>
-            <div>      <div className="m-2 float-right"><MyModal  pass="Delete" button="login" form="log in " title="login karo" /> </div>
+            <div>      <div className="m-2 float-right"><MyModal  pass="Delete" button="login" form="log in " title="" /> </div>
               {/* <button className=" float-right hover:bg-blue-100 m-2 p-1 rounded-lg " > Delete
             <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
             </svg>
           </button> */}
-          <div className="m-2"><MyModal  pass="ADD" button="login" form="log in " title="login karo" /> </div>
+          <div className="m-2"><MyModal  pass="ADD" button="login" form="log in " title="" /> </div>
           {/* <button className=" hover:bg-blue-100 m-2 p-1 rounded-lg "> <svg xmlns="http://www.w3.org/2000/svg " className="h-9 w-9 " viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
           </svg> 
