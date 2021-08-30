bcce319baddd2a726cc986d3ec881836c5e89cdb
Amitoj Singh <amitojsingh366@gmail.com>
2021-08-31 00:36:54 +0530

    commit

--- packages/website/src/components/AdminProgressBar.tsx
+++ packages/website/src/components/AdminProgressBar.tsx
@@ -1,7 +1,7 @@
 import { FC, useEffect, useRef, useState } from "react";
 
 type ProgressBarOpts = {
-    progress?: number
+  progress?: number
 }
 
 const AdminProgressBar: FC<ProgressBarOpts> = ({ progress = 0 }) => {
@@ -10,19 +10,23 @@ const AdminProgressBar: FC<ProgressBarOpts> = ({ progress = 0 }) => {
   const [prog, setProg] = useState(0);
 
   useEffect(() => {
-      setInterval(() => {
-        setProg((p) =>
-          p < progress ? p + 0.75 : p
-        );
-      }, 10, 5000)
-    }, [])
+    setInterval(() => {
+      setProg((p) =>
+        p < progress ? p + 0.75 : p
+      );
+    }, 10, 5000)
+  }, [])
+
+  useEffect(() => {
+    console.log(progress)
+  }, [progress])
 
   return (
-      <div ref={barRef} className="flex m-0.5 h-4 mx-2  bg-gray-300 rounded-md ">
-          <div ref={progressRef} className="bg-blue-600 rounded-md progressbar-bar h-4 " style={{
-              width: `${prog}%`
-          }}></div>
-      </div>
+    <div ref={barRef} className="flex m-0.5 h-4 mx-2  bg-gray-300 rounded-md ">
+      <div ref={progressRef} className="bg-blue-600 rounded-md progressbar-bar h-4 " style={{
+        width: `${prog}%`
+      }}></div>
+    </div>
   )
 }
 
