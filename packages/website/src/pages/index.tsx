39dfdae48be08ef43cc731bb657c163fc45749bb
aakashdeep singh <aakashdeep01971@gmail.com>
2021-09-02 23:16:00 +0530

    nice

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <div>
             <Head>
-                <title>JassWind</title>
+                <title>login</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <header className="text-gray-600 body-font shadow-lg">
